219bb84b937988888bc9b3e51319d7f1acf664bc
mbric <mattbrickner@gmail.com>
2013-01-22 23:23:26 -0500

    testing travis CI

--- OST.DataTablesHelper.Tests/ModelBinderTest.cs
+++ OST.DataTablesHelper.Tests/ModelBinderTest.cs
@@ -8,7 +8,7 @@ namespace OST.DataTablesHelper.Tests
     public class ModelBinderTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanGeneratePropertiesModel()
         {
             var queryString = @"?sEcho=1&iColumns=3&sColumns=Name%2CPrimaryBillingAddress_City%2CPrimaryBillingAddress_State&iDisplayStart=0&iDisplayLength=10&mDataProp_0=Name&mDataProp_1=PrimaryBillingAddress_City&mDataProp_2=PrimaryBillingAddress_State&sSearch=&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&_=1358913249672";
             var binder = new DataTableModelBinder();
